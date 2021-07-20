598c8a816b6404eb4d88e93dec97d7fc9e921ae4
JalenYes <jalen.yes@gmail.com>
2021-07-20 17:04:48 -0400

    Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,44 +8,44 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/jalen-32x32.png' />
+            <link rel='shortcut icon' href='/Jalen-32x32.png' />
 
-            <link rel='icon' sizes='180x180' href='/jalen-180x180.png' />
+            <link rel='icon' sizes='180x180' href='/Jalen-180x180.png' />
             <link
               rel='icon'
               type='image/png'
               sizes='96x96'
-              href='/jalen-96x96.png'
+              href='/Jalen-96x96.png'
             />
             <link
               rel='icon'
               type='image/png'
               sizes='32x32'
-              href='/jalen-32x32.png'
+              href='/Jalen-32x32.png'
             />
             <link
               rel='icon'
               type='image/png'
               sizes='16x16'
-              href='/jalen-16x16.png'
+              href='/Jalen-16x16.png'
             />
 
             <link rel='manifest' href='/manifest.json' />
 
             <script
               async
-              src='https://www.googletagmanager.com/gtag/js?id=G-SNY4GPFNX2'
-            ></script>
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
             <script
               dangerouslySetInnerHTML={{
                 __html: `
-               window.dataLayer = window.dataLayer || [];
-               function gtag(){dataLayer.push(arguments);}
-               gtag('js', new Date());
-               gtag('config', 'G-SNY4GPFNX2', {
-                 page_path: window.location.pathname,
-               });
-           `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `
               }}
             />
           </Head>
